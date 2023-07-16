b6e68db6f08234ab3e5ebceea4354a7310af8556
George-Dros <georgedross25@gmail.com>
2023-07-16 12:57:46 +0300

    Updates contact email send

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request
 import requests
+import smtplib
 
 
 response = requests.get("https://api.npoint.io/2bbe3d82b601b0484edc")
 posts = response.json()
+
+OWN_EMAIL = "YOUR OWN EMAIL ADDRESS"
+OWN_PASSWORD = "YOUR EMAIL ADDRESS PASSWORD"
 
 app = Flask(__name__)
 
@@ -34,15 +38,18 @@ def contact():
         email = request.form['email']
         phone = request.form['phone']
         message = request.form['message']
-
-        print(name)
-        print(email)
-        print(phone)
-        print(message)
+        send_mail(name,  email, phone, message)
 
         return render_template("contact.html", msg_sent=True)
 
     return render_template("contact.html", msg_sent=False)
+
+def send_mail(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
 if __name__ == "__main__":
